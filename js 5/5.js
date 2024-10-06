9ca2228dc4e01505d4564fea0a89c5da4e7f595d
Chech <ivankuikov3333@gmail.com>
2024-10-06 22:29:12 +0300

    js5

---
+++ js 5/5.js
@@ -0,0 +1,26 @@
+const buttonleft   = document.getElementById(`left`)
+const buttonright  = document.getElementById(`right`)
+const countcontain = document.getElementById(`countcontain`)
+const slidesline   = document.getElementById(`slidesline`)
+   
+const colSlides = 4;
+
+
+let i = 0;
+buttonleft.addEventListener('click', function() {
+    if (! (i == 0)){
+        i++;
+        countcontain.innerText = -i +1;
+        console.log (+1)
+        slidesline.style.left = `${80 * i}vw`;
+    }
+})
+
+buttonright.addEventListener("click", function() {
+    if (! (-i == colSlides - 1)) {
+        i--;
+        countcontain.innerText = -i +1;
+        console.log(-1)
+        slidesline.style.left = `${80 * i}vw`;
+    }
+})
