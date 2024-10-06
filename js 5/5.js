2353717ead8d962366bea5515e0b66602896902d
Chech <ivankuikov3333@gmail.com>
2024-10-06 22:44:54 +0300

    js 5

--- js 5/5.js
+++ js 5/5.js
@@ -1,26 +1,34 @@
-const buttonleft   = document.getElementById(`left`)
-const buttonright  = document.getElementById(`right`)
-const countcontain = document.getElementById(`countcontain`)
-const slidesline   = document.getElementById(`slidesline`)
-   
-const colSlides = 4;
+// Получение элементов из DOM по их идентификаторам
+const buttonleft = document.getElementById(`left`); // Кнопка для перехода влево
+const buttonright = document.getElementById(`right`); // Кнопка для перехода вправо
+const countcontain = document.getElementById(`countcontain`); // Контейнер для отображения номера текущего слайда
+const slidesline = document.getElementById(`slidesline`); // Контейнер для слайдов
 
+const colSlides = 4; // Общее количество слайдов (количество колонок)
 
+// Переменная для отслеживания текущего слайда
 let i = 0;
-buttonleft.addEventListener('click', function() {
-    if (! (i == 0)){
-        i++;
-        countcontain.innerText = -i +1;
-        console.log (+1)
-        slidesline.style.left = `${80 * i}vw`;
-    }
-})
 
-buttonright.addEventListener("click", function() {
-    if (! (-i == colSlides - 1)) {
-        i--;
-        countcontain.innerText = -i +1;
-        console.log(-1)
+// Обработчик события для кнопки "влево"
+buttonleft.addEventListener('click', function() {
+    // Если текущий индекс не равен 0 - мы можем перейти на предыдущий слайд
+    if (!(i == 0)) {
+        i++; // Увеличиваем индекс
+        countcontain.innerText = -i + 1; // Обновляем текст с номером текущего слайда
+        console.log(+1); // Логируем - это может быть полезно для отладки
+        // Сдвигаем контейнер со слайдами влево на 80% ширины окна просмотра
         slidesline.style.left = `${80 * i}vw`;
     }
-})
+});
+
+// Обработчик события для кнопки "вправо"
+buttonright.addEventListener("click", function() {
+    // Если текущий индекс не соответствует последнему слайду - мы можем перейти на следующий слайд
+    if (!(-i == colSlides - 1)) {
+        i--; // Уменьшаем индекс
+        countcontain.innerText = -i + 1; // Обновляем текст с номером текущего слайда
+        console.log(-1); // Логируем - это может быть полезно для отладки
+        // Сдвигаем контейнер со слайдами вправо на 80% ширины окна просмотра
+        slidesline.style.left = `${80 * i}vw`;
+    }
+});
