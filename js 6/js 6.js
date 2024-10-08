3a65c8b90274a9dfbbd9b65c342761630dfc9faa
Chech <ivankuikov3333@gmail.com>
2024-10-08 14:15:54 +0300

    js 6

--- js 6/js 6.js
+++ js 6/js 6.js
@@ -10,38 +10,45 @@ function moveCalc(event){
 
 let startX, startY;
 
-drag.addEventListener('mousemove', function(event){
+drag.addEventListener('mousedown', function(event) {
     startX = event.clientX - calc.offsetLeft;
     startY = event.clientY - calc.offsetTop;
-    document.addEventListener(' mousemove', moveCalc)
-})
+    document.addEventListener('mousemove', moveCalc);
+});
 
-drag.addEventListener('mouseup', function(){  
-    document.removeEventListener('mouseup', moveCalc)
-})
+
+document.addEventListener('mouseup', function() {
+    document.removeEventListener('mousemove', moveCalc);
+});
 
 
 let formula = '';
 
-buttonContain.addEventListener('click', function(event){
-    const value = event.target.value;
-    if (event.target.value == '=') {
-        formula = new Function('return' + formula)();
-        output.innerText = formula;
+buttonContain.addEventListener('click', function(event) {
+    const value = event.target.innerText; // Получаем текст на кнопке
+
+    if (value === '=') {
+        // Проверка формулы перед выполнением
+        if (/^[\d\s()+\-*/.]+$/.test(formula) && formula !== '') {
+            // Создание функции для вычисления выражения
+            const result = new Function('return ' + formula)();
+            output.innerText = result; // Вывод результата
+            formula = ''; // Сброс формулы после вычисления
+        } else {
+            output.innerText = 'Error'; // Для недопустимых формул
+            formula = ''; // Сброс формулы
+        }
+    } else if (value === '+/-') {
+        formula = (-1 * parseFloat(formula)).toString(); // Изменение знака числа
+        output.innerText = formula; // Обновление вывода
+    } else if (value === 'AC' || value === 'C') {
+        formula = ''; // Сброс формулы
+        output.innerText = '0'; // Обновление вывода
+    } else {
+        formula += value; // Добавление значения к формуле
+        output.innerText = formula; // Обновление вывода
     }
-    else if (value == '+/-'){
-        formula = -formula;
-        output.innerText = formula;
-    }
-    else if (value == 'AC' ||value == 'C'){
-        formula = '';
-        output.innerText = '0';
-    }
-    else {
-        formula += value;
-        output.innerText = formula;
-    }
-}) 
+});
 
 
 
