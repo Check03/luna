94880f846556464a73387fff0b05cbccdb44288c
Chech <ivankuikov3333@gmail.com>
2024-10-10 15:00:39 +0300

    js 7

---
+++ js7/script.js
@@ -0,0 +1,26 @@
+const colorsContain=document.getElementById('colorsContain');
+const text=document.getElementById('text');
+const color=document.getElementById('color');
+
+let colorsBank=[];
+
+document.getElementById('save').addEventListener('DOMContentLoaded',function(){
+    let cookiGet = Cookies.get('colors');
+    if(cookiGet){
+        colorsBank = JSON.parse(cookiGet);
+        for(item of colorsBank){
+            colorsContain.innerHTML += '<div class="color" style="background-color:'+item[1]+'"><div class="info"><h5 id="name">'+item[0]+'</h5><h5 id="color">'+item[1]+'</h5></div></div>';
+        }
+    }
+})
+
+document.getElementById('save').addEventListener('click',function(){
+    colorsBank.push([text.value, color.value]);
+    Cookies.set('colors', JSON.stringify(colorsBank));
+    colorsContain.innerHTML += '<div class="color" style="background-color:'+color.value+'"><div class="info"><h5 id="name">'+text.value+'</h5><h5 id="color">'+color.value+'</h5></div></div>';
+})
+
+document.getElementById('clear').addEventListener('click',function(){
+    colorsContain.innerHTML = '';
+    Cookies.remove('colors');
+})
